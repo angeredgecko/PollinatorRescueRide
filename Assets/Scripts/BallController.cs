20c60a84c193006957d5abe2a700324f116f64c4
angeredgecko <greg.zbo3@gmail.com>
2018-05-09 16:32:10 -0400

    test

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -55,7 +55,7 @@ public class BallController : MonoBehaviour {
             GameData.panel.GetComponent<PanelAnim>().playPopup();
             GameData.scoreAnim.playBig();
         }
-
+		//test
 
     }
 
